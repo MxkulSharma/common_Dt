6853d4c8938c5fac91b435c8aeae195005fcf3c1
Danny Lin <danny@kdrag0n.dev>
2024-03-07 11:07:14 +0530

    sm7325-common:Flatten APEXs for performance * PEXs have quite a bit of overhead in updatable form: they are ext4 filesystems mounted using loopback block devices, stored on yet another ext4 filesystem (system or vendor). They also increase boot time substantially due to needing to set up and mount 27 images with AVB. On a Pixel 2 XL, apexd-bootstrap took 485 ms during boot, and apexd took 471 ms for a total of 955 ms — nearly a second — spent on APEX setup, which is quite significant for modern devices. At least 422 ms out of that was spent blocking the rest of the system's initialization, but the overall slowdown is likely greater due to increased system load.
    * Updatable APEXs likely also come with a non-negligible amount of memory
    overhead due to setting up and mounting so many filesystem images, as
    well as AVB and potential double-caching effects caused by having two
    layers of filesystems in use.
    
    * Being able to update APEXs is useless for us anyway because we don't
    use Google's prebuilt modules and we have no other way to deliver APEX
    updates. Even if we were able to use Google's modules, we still want to
    be able to modify many of the APEX modules, so we need to build them
    from source.
    
    * Flattened APEXs have negligible overhead, since they're just stored as
    directories in /<partition>/apex and bind-mounted to /apex/<pkg name>.
    
    Signed-off-by: MxkulSharma <Sharmamukul06175@gmail.com>

--- init/init_sm7325.cpp
+++ init/init_sm7325.cpp
@@ -74,4 +74,6 @@ void vendor_load_properties()
     property_override_quad("ro.product.device", "ro.product.vendor.device", "ro.product.product.device", "ro.product.odm.device", device.c_str());
     property_override_quad("ro.product.name", "ro.product.vendor.name", "ro.product.product.name", "ro.product.odm.name", name.c_str());
     property_override("ro.build.product", device.c_str());
+     // Misc
+    property_override("ro.apex.updatable", "false");
 }
